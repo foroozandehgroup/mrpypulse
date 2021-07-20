ac4b284bc5be54d0c38cd069ce3857096d95d1a2
JB-V <57716353+JB-V@users.noreply.github.com>
2021-07-20 11:55:42 +0100

    Pulse __setattr__ test

--- tests/test_pulse.py
+++ tests/test_pulse.py
@@ -1,18 +1,30 @@
 import numpy as np
 import pytest
-#from mrpypulse.pulse import Pulse
+import copy
 import sys
 sys.path.append('..')
 import pulse
 
-def test_pulse_init():
+def random_pulse():
+    """Generates a random pulse
+    Returns
+    -------
+    p: pulse object
+        random pulse
     """
-    """
-    # random pulse
     ns = np.random.randint(2,1000)
     x = np.random.uniform(low=-1.0, high=1.0, size=ns)
     y = np.random.uniform(low=-1.0, high=1.0, size=ns)
     tres = np.random.rand()
+    return pulse.Pulse(x=x, y=y, ns=ns, tres=tres)
+
+def test_pulse_init():
+    # random pulse parameters
+    ns = np.random.randint(2,1000)
+    x = np.random.uniform(low=-1.0, high=1.0, size=ns)
+    y = np.random.uniform(low=-1.0, high=1.0, size=ns)
+    tres = np.random.rand()
+
     p1 = pulse.Pulse(x=x, y=y, ns=ns, tres=tres)
 
     # assert np/tres/tp produce same output
@@ -40,7 +52,24 @@ def test_pulse_init():
         p5=pulse.Pulse(x=x, ns=ns, tres=tres)
     
 
-def pulse_setattr_test():
-    """
-    """
-    # assert change in attribute values
+def test_pulse_setattr():
+    p1 = random_pulse()
+    p2 = copy.deepcopy(p1)
+
+    # assert phi0 change
+    p2.phi0 = np.random.uniform(low=-np.pi, high=np.pi)
+    p1.ph += p2.phi0
+    assert p1==p2
+
+    # assert w1 change
+    p2.w1 = np.random.uniform(low=0.1, high=3)
+    p1.r = p2.w1 * p1.r / np.max(p1.r)
+    assert p1==p2
+
+    # assert change of coordinates
+    p1.x = p1.x * np.random.uniform(low=-1, high=1)
+    p1.y = p1.y * np.random.uniform(low=-1, high=1)
+    assert p1!=p2
+    p1.r = p2.r
+    p1.ph = p2.ph
+    assert p1==p2
