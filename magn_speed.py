7165fec5a098d71c193853e6001ffd5972c8d52d
JB-V <jean-baptiste.verstraete@spc.ox.ac.uk>
2021-08-13 17:20:53 +0100

    magnetization speed test for simulation and simulation2

--- magn_speed.py
+++ magn_speed.py
@@ -2,33 +2,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pulse as pulse
-from simulation import excitation_profile
-import time as time
+import simulation
+import sequence as seq
+import time
 
-# hard pulse
-p1 = pulse.Pulse(phi0=np.pi/2, tp=14e-6, ns=2, r=np.array([15e3, 15e3]), ph=np.array([0, 0]), ID="hard 90­°")
 
-# HS pulses
-p2 = pulse.Parametrized(AM="WURST", FM="chirp", tp=500e-6, Q=5., bw=300e3, tres=0.5e-6)
-p3 = pulse.Parametrized(AM="tanh", FM="sech", tp=500e-6, Q=5., bw=300e3, ns=1000)
-p2.phi0 = np.pi/2
+# comparison between simulate and simulate2
 
-ns = np.random.randint(2,1000)
-x = np.random.uniform(low=-1.0, high=1.0, size=ns)
-y = np.random.uniform(low=-1.0, high=1.0, size=ns)
-tres = np.random.rand()
+t90min = 100e-6
+t180min = 100e-6
+bw = 500e3
+tres = 2e-6
+chorus = seq.Exc_3fs(t90min, t180min, bw, tres, plot=False, polyfit=True)
 
-tp = ns * tres
-p4 = pulse.Pulse(x=x, y=y, tp=tp, tres=tres, ID="random pulse")
-
-p4.plot(title=p4.ID)
-
-plt.figure()
-plt.subplot(1,2,1)
-p2.plot(form='polar')
-plt.subplot(1,2,2)
-p2.plot(form='Cartesian')
+# check that magnetization is recocused in centre part
+off = np.linspace(-0.5*bw, 0.5*bw, 100)
+start = time.time()
+magn, off = simulation.simulate(chorus.pulses, offsets=off, pc=chorus.pc)
+print(f"simulate computation time: {format(time.time() - start,'0.2f')}s")
+simulation.plot_magn(magn, off)
 
 start = time.time()
-excitation_profile([p2], p2.bw, 21)
-print(f"Computation time: {format(time.time() - start,'0.2f')}s")
+start = time.time()
+magn, off = simulation.simulate2(chorus.pulses, offsets=off, pc=chorus.pc)
+print(f"simulate computation time: {format(time.time() - start,'0.2f')}s")
+simulation.plot_magn(magn, off)
