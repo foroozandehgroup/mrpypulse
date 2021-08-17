361e4a7b777e8b89fa4a3809f3e717e00b138234
JB-V <jean-baptiste.verstraete@spc.ox.ac.uk>
2021-08-17 16:09:29 +0100

    chorus example minor update

--- example_chorus.py
+++ example_chorus.py
@@ -9,5 +9,8 @@ bw = 300e3
 tres = 0.5e-6
 start = time.time()
 chorus = seq.Exc_3fs(t90min, t180min, bw, tres, 
-                     plot=True, t_del=0, polyfit=True)
+                     plot=True, t_del=0, polyfit=True, 
+                     pulse_args={"sm": 12.5},
+                     polyfit_args={"deg": 4})
+
 print(f"Computation time: {format(time.time() - start,'0.2f')}s")
