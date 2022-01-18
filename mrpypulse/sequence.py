ab2a8b2bed86f0c596dc9794f806265a040de58f
JB-V <jean-baptiste.verstraete@spc.ox.ac.uk>
2022-01-18 18:41:02 +0000

    Sequence additional functionalities

--- mrpypulse/sequence.py
+++ mrpypulse/sequence.py
@@ -1,22 +1,54 @@
 """ This module contains the class Sequence."""
 
+import os
+import copy
 import numpy as np
 from . import pulse
 from .magnetization import simulate, magn_phase, plot_magn
 import matplotlib.pyplot as plt
 
 
-def pc_rec(phase_cycles, CTP):
+def pc_rec(phase_cycles, ctp):
     """
-    Computes receiver phase with the phase cycling pc
+    Computes receiver phase
+
     Parameters
     ----------
     phase_cyles: numpy array of floats
-        array containing the phase cyle of each pulse on its line
-    CTP: 1D-numpy array of floats
+        array containing the phase cycle of each pulse on its line
+    ctp: 1D-numpy array of floats
         vector containing the coherence transfer pathway of the pulses
-    TODO code function
+
+    Returns
+    -------
+    ph_rec
+        array containing the phase cycle of the receiver
+
+    example: 90  -  180 - 180 - detection
+
+    ctp = [-1, +2, -2]
+    +1                 ******
+    0 **********      *      *
+    -1          ******        *************
+
+    ph_pulses = [ph1, ph2, ph3]
+
+    ph1 =    0 0 0 0 0 0 0 0 2 2 2 2 2 2 2 2
+    ph2 =    0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1
+    ph3 =    0 0 1 1 2 2 3 3 0 0 1 1 2 2 3 3
+    ph_rec = 0 2 2 0 0 2 2 0 2 0 0 2 2 0 0 2
     """
+    if len(phase_cycles) != len(ctp):
+        raise IndexError("ctp list length not equal to number of pulses")
+
+    ph_rec = np.zeros(len(phase_cycles[0]))
+
+    for i, elem in enumerate(phase_cycles):
+        ph_rec += elem * ctp[i]
+
+    ph_rec = ph_rec % 4  # modulation
+
+    return ph_rec.astype(int)
 
 
 class Sequence:
@@ -25,7 +57,7 @@ class Sequence:
     Class representing a pulse sequence
     """
 
-    def __init__(self, pulses, total_time: float = None, pc=None):
+    def __init__(self, pulses, total_time=None, pc=None, ID=None):
         """
         Parameters
         ----------
@@ -36,6 +68,11 @@ class Sequence:
         pc: numpy array of floats
             phase cycling tables of the sequence
         """
+        if ID is not None:
+            self.ID = ID
+        else:
+            self.ID = 'unnamed_seq'
+
         self.pulses = pulses
 
         if total_time is None:
@@ -54,18 +91,6 @@ class Sequence:
 
         if pc is not None:
             self.pc = pc
-
-        # TODO: dicuss addition of list of pulse duration/delays
-        # (can be deduced from start/stop in other functions)
-
-    def __add__(self, sequence2add):
-        """
-        TODO code method
-        """
-        # check overlap?
-        # pulses = [self.pulses, sequence2add.pulses]
-        # sequence_sum = sequence(pulses, total_time=total_time)
-        # return sequence_sum
 
     def __eq__(self, seq):
         """
@@ -93,7 +118,7 @@ class Sequence:
 
         return True
 
-    def insert(self, elem, position):
+    def insert(self, position, elem):
         """
         Parameters
         ----------
@@ -101,27 +126,44 @@ class Sequence:
             element to insert in the sequence (pulse or delay)
         position: int
             where the pulse/delay should be inserted in the pulse list
-        TODO code method
         """
         if isinstance(elem, pulse.Pulse):
-            # insert pulse in pulse list
-            # update total time and pulse positions
-            pass
+
+            # insert pulse in pulses list
+            if position == len(self.pulses):
+                elem.start = self.pulses[-1].end
+            else:
+                elem.start = self.pulses[position].start
+            self.pulses.insert(position, elem)
+
+            # pulses position shift
+            for i in range(len(self.pulses)):
+                if i > position:
+                    self.pulses[i].start += elem.tp
+
+            self.total_time += elem.tp
+
+            if self.w1max < elem.w1:
+                self.w1max = elem.w1
+
         elif isinstance(elem, float):  # delay case
-            # update total time and pulse positions
-            pass
-        else:
-            raise ValueError('A pulse (Pulse objects) or a delay (float) '
-                             'should be inserted.')
+
+            # pulses position shift
+            for i in range(len(self.pulses)):
+                if i >= position:
+                    self.pulses[i].start += elem
+
+            # update total time
+            self.total_time += elem
 
     def append(self, elem):
         """
         Parameters
         ----------
         elem: pulse object or float
-            element to insert in the sequence (pulse or delay)
+            element to insert in the sequence (pulse or delay in s)
         """
-        self.insert(elem, len(self.pulses))
+        self.insert(len(self.pulses), elem)
 
     def pulses_sum(self):
         """
@@ -134,44 +176,137 @@ class Sequence:
         Plots the sequence pulses
         """
         p = sum(self.pulses)
-        # TODO add potential final delay
+
+        # potential initial delay
+        if not np.isclose(0, self.pulses[0].start, rtol=1e-6, atol=1e-15):
+            delay = pulse.NoPulse(tp=self.pulses[0].start,
+                                  tres=self.tres, start=0)
+            p += delay
+
+        # potential final delay
+        if not np.isclose(p.tp, self.total_time, rtol=1e-6, atol=1e-15):
+            delay = pulse.NoPulse(tp=self.total_time - p.tp,
+                                  tres=self.tres, start=p.tp)
+            p += delay
+
         p.plot()
+        plt.xlim(0, self.total_time)
 
-    def seq2TopSpin(self):
+    def seq2topspin(self, path=None):
         """
-        Export sequence pulse shapes to TopSpin waveforms
-        TODO code method
-        """
+         Export sequence to folder of name self.ID containing TopSpin shape
+         files
 
-    def seq2Xepr(self):
+        Parameters
+        ----------
+        path: string
+            where the folder with the .shp files is created, default is
+            current directory
         """
-        Export sequence pulse shapes to Xepr waveforms
-        TODO code method
+        if path is None:
+            path = os.getcwd()
+        path = os.path.join(path, self.ID)
+
+        # create folder to store shape files
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        # export pulse shapes
+        for p in self.pulses:
+            p.topspin_file(path)
+
+    def seq2xepr(self, path=None, shp_nb=400):
         """
-        # export each pulse with phase cycling options
-        # plot each pulse and export its figure
+        Export sequence to folder of name self.ID containing Xepr shape files
+
+        Parameters
+        ----------
+        shp_nb: int
+            shape number used for file names and number
+        path: string
+            where the folder with the .shp files is created, default is
+            current directory
+
+        Generates the shape with 4 different phase for possible phase cycling
+        (0, pi/2, pi, 3*pi/4).
+        """
+        if path is None:
+            path = os.getcwd()
+        path = os.path.join(path, self.ID)
+
+        # create folder to store .shp files
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        # export pulse shape files
+        pulses = copy.deepcopy(self.pulses)
+        shp_paths = []
+        for p in pulses:
+
+            for phi0 in [0., np.pi/2, np.pi, 3*np.pi/2]:
+
+                p.phi0 = phi0  # add phase cycle
+                p.xepr_file(shp_nb, path)
+
+                # add shape file name to shp_paths
+                shp_paths.append(os.path.join(
+                                 path, str(shp_nb) + '.shp'))
+                shp_nb += 1
+
+        shps_path = os.path.join(path, str(shp_nb) + '_merged.shp')
+
+        # merge shape files together
+        with open(shps_path, 'w') as merged_file:
+            for names in shp_paths:
+                with open(names) as infile:
+                    merged_file.write(infile.read())
 
 
 class Exc_3fs(Sequence):
 
     """
     Class representing an excitation from 3 frequency-swept pulses (CHORUS,
-                                                                   ABSTRUSE).
-    # class or function?
+                                                                   ABSTRUSE)
+    Parameters
+    ----------
+    t90min
+        minimal excitation pulse duration (s)
+    t180min
+        minimal refocusing pulse duration (s)
+    bw
+        bandwidth of the sequence (Hz)
+    tres
+        time resolution of the sequence (s)
+    Q_exc
+        adiabaticity factor of the excitation pulse (default, 0.441)
+    Q_ref
+        adiabaticity factor of the refocusing pulses (default, 5)
+    pulse_args
+    t_del: float
+        delay used to extend the sequence duration, placed before and after the
+        last pulse
+    polyfit: boolean
+        to apply polynomial fitting of the phase
+    polyfit_args: dict
+        polynomial fitting parameters (cf. pulse.Parametrized.add_ph_polyfit())
+    plot: boolean
+        to plot the sequence, its simulatin and potential polynomial fitting
+        operation
+    ID: string
+        cf. Sequence (default 'exc_3fs')
     """
 
     def __init__(self, t90min, t180min, bw, tres,
                  Q_exc=0.4412712003053, Q_ref=5, pulse_args={}, t_del=0,
                  polyfit=False, polyfit_args={},
-                 plot=False):
+                 plot=False, ID='exc_3fs'):
 
         p1 = pulse.Parametrized(tp=t90min, bw=bw, Q=Q_exc,
-                                tres=tres, **pulse_args)
+                                tres=tres, ID='p1', **pulse_args)
         p2 = pulse.Parametrized(tp=t180min+t90min/2, bw=bw, Q=Q_ref,
-                                tres=tres, start=t90min, **pulse_args)
-        p3 = pulse.Parametrized(tp=t180min, bw=bw, Q=Q_ref,
-                                tres=tres, start=p2.end + t90min/2 + t_del,
-                                **pulse_args)
+                                tres=tres, ID='p2', start=t90min, **pulse_args)
+        p3 = pulse.Parametrized(tp=t180min, bw=bw, Q=Q_ref, tres=tres, ID='p3',
+                                start=p2.end + t90min/2 + t_del, **pulse_args)
 
         pulses = [p1, p2, p3]
 
@@ -179,12 +314,11 @@ class Exc_3fs(Sequence):
         pc2 = np.array([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3])
         pc3 = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3])
 
-        # pc31 = pc_rec([pc1, pc2, pc3], [-1, +2, -2])
+        pc31 = pc_rec([pc1, pc2, pc3], [-1, +2, -2])
 
-        pc31 = np.array([0, 2, 0, 2, 2, 0, 2, 0, 0, 2, 0, 2, 2, 0, 2, 0])
+        # pc31 = np.array([0, 2, 0, 2, 2, 0, 2, 0, 0, 2, 0, 2, 2, 0, 2, 0])
         pc = np.pi/2*np.stack((pc1, pc2, pc3, pc31))
 
-        # simulation
         if polyfit:
             limit = 0.5*pulses[0].bw
             off = np.linspace(-0.5*pulses[0].bw+p1.delta_f,
@@ -196,7 +330,7 @@ class Exc_3fs(Sequence):
 
             p1.add_ph_polyfit(magn_phase(magn), **polyfit_args, plot=plot)
 
-        Sequence.__init__(self, pulses, p3.end+t_del, pc)
+        Sequence.__init__(self, pulses, total_time=p3.end+t_del, pc=pc, ID=ID)
 
         self.t90min = t90min
         self.t180min = t180min
@@ -210,7 +344,6 @@ class Exc_3fs(Sequence):
         self.polyfit_args = polyfit_args
 
         if plot:
-            # simulation
             limit = 0.5*pulses[0].bw
             off = np.linspace(-limit, limit, 100)
             magn = simulate(self.pulses, off, pc=self.pc)
--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -1,6 +1,65 @@
 import numpy as np
-from mrpypulse import magnetization
-from mrpypulse import sequence
+import copy
+from mrpypulse import pulse, sequence, magnetization
+
+
+def test_pc_rec():
+    pc1 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    pc2 = np.array([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3])
+    pc3 = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3])
+
+    ph_rec = sequence.pc_rec([pc1, pc2, pc3], [-1, +2, -2])
+
+    pc31 = np.array([0, 2, 0, 2, 2, 0, 2, 0, 0, 2, 0, 2, 2, 0, 2, 0])
+
+    assert np.all(ph_rec == pc31)
+
+    pc1 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2])
+    pc2 = np.array([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1])
+    pc3 = np.array([0, 0, 1, 1, 2, 2, 3, 3, 0, 0, 1, 1, 2, 2, 3, 3])
+
+    ph_rec = sequence.pc_rec([pc1, pc2, pc3], [-1, +2, -2])
+
+    pc31 = np.array([0, 2, 2, 0, 0, 2, 2, 0, 2, 0, 0, 2, 2, 0, 0, 2])
+
+    assert np.all(ph_rec == pc31)
+
+
+def test_sequence_insert():
+    t90min = 80e-6
+    t180min = 101e-6
+    bw = 506e3
+    tres = 0.5e-6
+    chorus = sequence.Exc_3fs(t90min, t180min, bw, tres,
+                              plot=False, polyfit=False, t_del=80e-6)
+    chorus.insert(0, 100e-6)
+    p = pulse.Parametrized(AM="sech", FM="sech",
+                           tp=500e-6, Q=5., bw=300e3, ns=1000)
+    chorus.insert(1, p)
+
+    chorus2 = sequence.Exc_3fs(t90min, t180min, bw, tres,
+                               plot=False, polyfit=False, t_del=0e-6)
+
+    chorus2.insert(0, 100e-6)
+    chorus2.insert(2, 80e-6)
+    chorus2.insert(3, 80e-6)
+    chorus2.insert(1, p)
+
+    assert chorus == chorus2
+
+
+def test_sequence_append():
+    chorus = sequence.Exc_3fs(t90min=79e-6, t180min=180e-6, bw=487.52e3,
+                              tres=0.5e-6, plot=False, polyfit=False)
+    p = pulse.Parametrized(AM="sech", FM="sech",
+                           tp=500e-6, Q=5., bw=300e3, ns=1000)
+    chorus2 = copy.deepcopy(chorus)
+    chorus2.insert(3, 100e-6)
+    chorus2.insert(3, p)
+    chorus.append(100e-6)
+    chorus.append(p)
+
+    assert chorus == chorus2
 
 
 def test_Exc_3fs_init():
@@ -8,7 +67,7 @@ def test_Exc_3fs_init():
     Generates a short chorus with a low time resolution and test that the
     magnetization is grossly refocused on y.
     """
-    t90min = 100e-6
+    t90min = 80e-6
     t180min = 101e-6
     bw = 506e3
     tres = 2e-6
