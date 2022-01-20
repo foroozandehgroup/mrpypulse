0bcf6350a83215e193bf72d48c555cf3daea737a
JB-V <jean-baptiste.verstraete@spc.ox.ac.uk>
2022-01-20 12:13:37 +0000

    gitignore and PEP8 test

---
+++ tests/test_PEP8.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pycodestyle
+
+
+def test_style_mrpypulse():
+    s = pycodestyle.StyleGuide()
+    p = (Path(__file__).parents[1].resolve()) / "mrpypulse"
+    res = s.check_files([f for f in p.iterdir() if f.suffix == ".py"])
+    assert res.total_errors == 0
+
+
+def test_style_tests():
+    s = pycodestyle.StyleGuide()
+    p = Path(__file__).parent
+    res = s.check_files([f for f in p.iterdir() if f.suffix == ".py"])
+    assert res.total_errors == 0
