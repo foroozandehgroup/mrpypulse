6991f2136a6932cda819237d657bbe3a74267f8b
JB-V <jean-baptiste.verstraete@spc.ox.ac.uk>
2022-01-20 14:18:08 +0000

    python 3.6 and 3.10 added to tests

--- setup.py
+++ setup.py
@@ -18,9 +18,11 @@ setup(
     url="https://github.com/foroozandehgroup/mrpypulse",
     packages=find_packages(exclude=["tests"]),
     classifiers=[
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License"
         "Operating System :: OS Independent",
     ],
